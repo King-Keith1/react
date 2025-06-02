714d8eb25442002eec970b08fafab07735b8db51
King-Keith1 <keith4321ben@gmail.com>
2025-06-02 11:38:49 +0200

    update

--- my_code/Hooks/chap01/react-hooks-chapter1/src/Greeting.jsx
+++ my_code/Hooks/chap01/react-hooks-chapter1/src/Greeting.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Greeting() {
   return (
     <div>
-      <h2>Welcome to the App!</h2>
+      <h2>Welcome my hook App!</h2>
       <p>
         Brought to you by <span style={{ backgroundColor: 'purple', fontWeight: 'bold' }}>codecollege.co.za</span>
       </p>
