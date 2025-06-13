f66ae1e38df5f22772e37bdc4cd16e75d509971a
King-Keith1 <keith4321ben@gmail.com>
2025-06-13 14:41:24 +0200

    commit

---
+++ my_code/chapter1/my-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello World! This is my first React app.
+        I have added some text to the default template.
+        I am learning React and this is my first step.
+        Fight the good Fight!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
